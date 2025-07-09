25a46944a5107430804c93df271219daf72f7e5c
YTase-source <yota.tase@giftee.co>
2025-07-09 09:07:50 +0000

    Finish Chapter6_Exercise 6.3.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255}, format: { with: VALID_EMAIL_REGEX}, uniqueness: true
+
+  # ハッシュ化するメソッド
+  has_secure_password
+
+  validates :password, presence: true, length: { minimum: 8 }
 end
