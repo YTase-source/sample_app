403731dff2f5e42c9e1c2df364427cb943f7527e
yota.tase <yota.tase@yotatasenoMacBook-Pro.local>
2025-07-17 14:36:34 +0900

    Finish Chapter8 Exercise 8.2.6

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -10,6 +10,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
+  test 'login with valid email/invalid password' do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'invalid' } }
+    assert_response :unprocessable_entity
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
   # 無効なログイン情報をPOSTした際の挙動確認
   test 'login with invalid information' do
     # ログイン用のパスを開く
