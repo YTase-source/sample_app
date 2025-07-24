2cbac5fc1b5989161967be03ff94f4ba06220daa
yota.tase <yota.tase@yotatasenoMacBook-Pro.local>
2025-07-24 17:31:16 +0900

    Improve users update action

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   # ハッシュ化するメソッド
   has_secure_password
 
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   class << self
     # 渡された文字列のハッシュ値を返す
