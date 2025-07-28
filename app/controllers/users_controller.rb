bd36fe9ced6abdf7c18edf0af076dda04aa670ad
yota.tase <yota.tase@yotatasenoMacBook-Pro.local>
2025-07-28 14:48:25 +0900

    Add a friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: %i[edit update]
+  before_action :correct_user, only: %i[edit update]
 
   def show
     @user = User.find(params[:id])
@@ -25,12 +26,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = 'Profile updated'
       redirect_to @user
@@ -52,7 +50,15 @@ class UsersController < ApplicationController
   def logged_in_user
     return if logged_in?
 
+    # ログイン済みでない場合は、直前のURLを記憶してログインページへリダイレクト
+    store_location
     flash[:danger] = 'Please log in.'
     redirect_to login_url, status: :see_other
+  end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
   end
 end
