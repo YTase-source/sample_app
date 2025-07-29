5b4bca08d20060c2f7df33672edb379234908fdf
yota.tase <yota.tase@yotatasenoMacBook-Pro.local>
2025-07-29 13:53:07 +0900

    Finish Chapter10 Exercise10.3.1

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,19 +1,54 @@
-require "test_helper"
+require 'test_helper'
+require 'helpers/application_helper_test'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'layout links' do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
     get contact_path
-    assert_select "title", full_title("Contact")
+    assert_select 'title', full_title('Contact')
     get signup_path
-    assert_select "title", full_title("Sign up")
+    assert_select 'title', full_title('Sign up')
+  end
+
+  def check_header_link
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', users_path
+
+    # ドロップダウンメニュー
+    if is_logged_in?
+      assert_select 'a[href=?]', user_path(@user)
+      assert_select 'a[href=?]', edit_user_path(@user)
+      assert_select 'a[href=?]', logout_path
+    else
+      assert_select 'a[href=?]', login_path
+    end
+  end
+
+  def check_footer_link
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', 'https://news.railstutorial.org/'
+  end
+
+  test 'users index layout links' do
+    # 未ログインの場合
+    get users_path
+    assert_redirected_to login_url
+
+    # ログイン済の場合
+    log_in_as(@user)
+    get users_path
+    assert check_header_link
+    assert check_footer_link
   end
 end
