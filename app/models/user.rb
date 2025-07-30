f9bca2626613b00969b9011cfa07d913124053bf
yota.tase <yota.tase@ip-10-32-1-67.ap-northeast-1.compute.internal>
2025-07-30 10:46:32 +0900

    Add column to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   # 記憶トークン用のローカル変数
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  # save前に小文字に変換する
-  # before_save { self.email = email.downcase }
-  before_save { email.downcase! }
+  # save前にemailを小文字に変換する
+  before_save :downcase_email
+  # create前に有効化seeds.rbトークンを暗号化する
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50, minimum: 5 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
@@ -59,5 +60,18 @@ class User < ApplicationRecord
   # ユーザーのログイン情報を破棄する
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  # メールアドレスをすべて小文字にする
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # 有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
