fc2cc692e0a4005ffbb074f069439a570ba7aa29
yota.tase <yota.tase@ip-10-32-1-67.ap-northeast-1.compute.internal>
2025-07-31 14:33:19 +0900

    Add account activation test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,10 +43,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   host = 'localhost'
+  port = '3000'
   # クラウドIDEの場合は以下をお使いください
   # config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   # localhostで開発している場合は以下をお使いください
-  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.default_url_options = { host: host, port: port, protocol: 'http' }
 
   config.action_mailer.perform_caching = false
 
