60f1bc70933a992128ef22bc34ffb794d86430fa
yota.tase <yota.tase@yotatasenoMacBook-Pro.local>
2025-08-04 10:19:40 +0900

    Add reset to user model

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,20 @@
 class PasswordResetsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      # パスワード再設定の属性を設定する
+      @user.create_reset_digest
+      # パスワード再設定のメールを送信する
+      @user.send_password_reset_email
+      flash[:info] = 'Email sent with password reset instructions'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'Email address not found'
+      render 'new', status: :unprocessable_entity
+    end
   end
 
-  def edit
-  end
+  def edit; end
 end
