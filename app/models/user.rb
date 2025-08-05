b463430def158e6c1f0597f214d83e8a431fbca2
yota.tase <yota.tase@yotatasenoMacBook-Pro.local>
2025-08-05 14:35:46 +0900

    Improve user test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
 
   # 記憶トークン用のローカル変数
   attr_accessor :remember_token, :activation_token, :reset_token
