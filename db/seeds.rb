8d374787d554580aec7f7652e15755608091018b
yota.tase <yota.tase@yotatasenoMacBook-Pro.local>
2025-08-05 15:39:48 +0900

    Add microposts seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,3 +26,11 @@ User.create!(name: 'Example User',
                activated: true,
                activated_at: Time.zone.now)
 end
+
+# ユーザーの一部を対象にマイクロポストを生成する
+users = User.order(:created_at).take(6)
+50.times do
+  # Faker::Lorem.sentence・・・ダミーのテキストを作成するgemメソッド
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
