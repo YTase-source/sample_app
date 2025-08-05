111eeb767e420be145d4ba14949edf1ed14e7b56
yota.tase <yota.tase@yotatasenoMacBook-Pro.local>
2025-08-05 15:10:20 +0900

    Generate microposts controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     redirect_to root_url and return unless @user.activated == true
+
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
