1e63ff24fe63296747b60912099be281cde273d1
yota.tase <yota.tase@yotatasenoMacBook-Pro.local>
2025-08-05 16:12:57 +0900

    Finish Chapter12 Exercise13.2.3

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -15,7 +15,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'h1>img.gravatar'
     # HTML内にマイクロポストの投稿数が存在するか確認
     assert_match @user.microposts.count.to_s, response.body
-    assert_select 'div.pagination'
+    assert_select 'div.pagination', count: 1
     # HTML内に各マイクロポストの内容が存在するか確認
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
