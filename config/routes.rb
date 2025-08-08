f27e5621c1cd023615f5a527269d5027547f83b1
yota.tase <yota.tase@yotatasenoMacBook-Pro.local>
2025-08-08 13:52:14 +0900

    Add a Following View and Followers View

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get     '/login',   to: 'sessions#new'
   post    '/login',   to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy'
-  resources :users do  
+  resources :users do
     member do
       get :following, :followers
     end
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: %i[new create edit update]
   resources :microposts,          only: %i[create destroy]
-  resources :relationships,       only: [:create, :destroy]
+  resources :relationships,       only: %i[create destroy]
   get '/microposts', to: 'static_pages#home'
 end
